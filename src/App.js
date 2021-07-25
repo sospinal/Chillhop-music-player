1ff9aeb62b3781e10d67547e0f0af409327d9d7b
Santiago Ospina Leal <sospinaleal@gmail.com>
2021-07-24 22:16:02 -0500

    A console.log in app.js is deleted.

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
     const roundedCurrent = Math.round(current);
     const roundedDuration = Math.round(duration);
     const animation = Math.round((roundedCurrent / roundedDuration) * 100);
-    console.log(animation);
+
     setSongInfo({
       ...songInfo,
       currentTime: current,
